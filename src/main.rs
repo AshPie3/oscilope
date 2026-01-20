ae32e557db2292e9a2bad6f3dae2898b26d9c16d
Nika <wonky.future@gmail.com>
2026-01-20 10:24:02 +0100

    changed to RUHear library and got stereo but no sound for me

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,41 @@
-use winit::event_loop::EventLoop;
+use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+use anyhow::Result;
+use std::sync::{Arc, Mutex};
+use ruhear::{rucallback, RUBuffers, RUHear};
+use std::thread;
 
-fn main() {
-    let amp = 1;
-    let mut phase: i32 = 0;
-    let freq = 2;
-    let mut y_val;
-    while true {
-        println!("Phase: {}", phase);
-        y_val = phase.sin(); 
+fn main() -> Result<()> {
+    //Set up device
+    //let host = cpal::default_host();
+    //let device = host.default_input_device().expect("failed to find input device");
+    // Set up config
+    //let config: cpal::StreamConfig = device.default_input_config()?.into();
+    // Set up input stream
+    //let input_stream = device.build_input_stream(&config, parse_stream, err_fn, None)?;
+    // Main loop
+    println!("Starting input stream ");
+    // ruhear capture audio output
+    let callback = |audio_buffers: RUBuffers| {
+        parse_stream(audio_buffers.clone());
+    };
+    let callback = rucallback!(callback);
+    let mut ruhear = RUHear::new(callback);
 
-        phase += 1;
-    }
+    ruhear.start();
+    std::thread::sleep(std::time::Duration::from_secs(100));
+    ruhear.stop();
 
-    
+    Ok(())
 }
 
-struct sine_wave {
-    amp: i32,
-    phase: i32,
-    freq: i32,
-
+fn err_fn(err: cpal::StreamError) {
+    eprintln!("an error occurred on stream: {err}");
 }
 
-fn sine_wave(amp: i32, phase: i32, freq: i32) -> i32 {
-4
-
+fn parse_stream(data: Vec<Vec<f32>>) {
+    println!("Samples: {:?}", data[0].len());
+    println!("Value Channel 0: {:?}", data[0].last());
+    println!("Value Channel 1: {:?}", data[1].last());
+    std::thread::sleep(std::time::Duration::from_secs(1));
 }
+
