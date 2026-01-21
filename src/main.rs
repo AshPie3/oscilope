1b8cb133bee32b508b4d8b330cdf09ad8bf7c5f5
Nika <wonky.future@gmail.com>
2026-01-21 21:00:15 +0100

    Starting to make the window

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,297 @@
-use core::f32;
-use std::sync::{Arc, Mutex};
+use core::f32;}}
+use std::{iter, sync::{Arc, Mutex}, usize};
 use ruhear::{rucallback, RUBuffers, RUHear};
 use std::sync::mpsc;
+use winit::{
+    application::ApplicationHandler, event::*, event_loop::{ActiveEventLoop, EventLoop}, keyboard::{KeyCode, PhysicalKey}, window::Window
+};
 
+#[cfg(target_arch = "wasm32")]
+use wasm_bindgen::prelude::*;
 
-#[derive(Clone)]
-struct Buffer {
-    x: Vec<f32>,
-    y: Vec<f32>,
+pub struct State {
+    surface: wgpu::Surface<'static>,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    config: wgpu::SurfaceConfiguration,
+    is_surface_configured: bool,
+    window: Arc<Window>,
+}
+impl State {
+    // ...
+    async fn new(window: Arc<Window>) -> anyhow::Result<State> {
+        let size = window.inner_size();
+
+        // The instance is a handle to our GPU
+        // BackendBit::PRIMARY => Vulkan + Metal + DX12 + Browser WebGPU
+        let instance = wgpu::Instance::new(&wgpu::InstanceDescriptor {
+            #[cfg(not(target_arch = "wasm32"))]
+            backends: wgpu::Backends::PRIMARY,
+            #[cfg(target_arch = "wasm32")]
+            backends: wgpu::Backends::GL,
+            ..Default::default()
+        });
+
+        let surface = instance.create_surface(window.clone()).unwrap();
+
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::default(),
+                compatible_surface: Some(&surface),
+                force_fallback_adapter: false,
+            })
+            .await?;
+        let (device, queue) = adapter
+            .request_device(&wgpu::DeviceDescriptor {
+                label: None,
+                required_features: wgpu::Features::empty(),
+                experimental_features: wgpu::ExperimentalFeatures::disabled(),
+                // WebGL doesn't support all of wgpu's features, so if
+                // we're building for the web we'll have to disable some.
+                required_limits: if cfg!(target_arch = "wasm32") {
+                    wgpu::Limits::downlevel_webgl2_defaults()
+                } else {
+                    wgpu::Limits::default()
+                },
+                memory_hints: Default::default(),
+                trace: wgpu::Trace::Off,
+            })
+            .await?;
+
+        let surface_caps = surface.get_capabilities(&adapter);
+        // Shader code in this tutorial assumes an sRGB surface texture. Using a different
+        // one will result in all the colors coming out darker. If you want to support non
+        // sRGB surfaces, you'll need to account for that when drawing to the frame.
+        let surface_format = surface_caps.formats.iter()
+            .find(|f| f.is_srgb())
+            .copied()
+            .unwrap_or(surface_caps.formats[0]);
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: surface_format,
+            width: size.width,
+            height: size.height,
+            present_mode: surface_caps.present_modes[0],
+            alpha_mode: surface_caps.alpha_modes[0],
+            view_formats: vec![],
+            desired_maximum_frame_latency: 2,
+        };
+
+        pub fn resize(&mut self, width: u32, height: u32) {
+            if width > 0 && height > 0 {
+            self.config.width = width;
+            self.config.height = height;
+            self.surface.configure(&self.device, &self.config);
+            self.is_surface_configured = true;
+            }
+        }
+
+        Ok(Self {
+            surface,
+            device,
+            queue,
+            config,
+            is_surface_configured: false,
+            window,
+        })
+    }
 }
 
+
+pub struct App {
+    #[cfg(target_arch = "wasm32")]
+    proxy: Option<winit::event_loop::EventLoopProxy<State>>,
+    state: Option<State>,
+}
+
+
+impl App {
+    pub fn new(#[cfg(target_arch = "wasm32")] event_loop: &EventLoop<State>) -> Self {
+        #[cfg(target_arch = "wasm32")]
+        let proxy = Some(event_loop.create_proxy());
+        Self {
+            state: None,
+            #[cfg(target_arch = "wasm32")]
+            proxy,
+        }
+    }
+}
+
+impl ApplicationHandler<State> for App {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        #[allow(unused_mut)]
+        let mut window_attributes = Window::default_attributes();
+
+        #[cfg(target_arch = "wasm32")]
+        {
+            use wasm_bindgen::JsCast;
+            use winit::platform::web::WindowAttributesExtWebSys;
+            
+            const CANVAS_ID: &str = "canvas";
+
+            let window = wgpu::web_sys::window().unwrap_throw();
+            let document = window.document().unwrap_throw();
+            let canvas = document.get_element_by_id(CANVAS_ID).unwrap_throw();
+            let html_canvas_element = canvas.unchecked_into();
+            window_attributes = window_attributes.with_canvas(Some(html_canvas_element));
+        }
+
+        let window = Arc::new(event_loop.create_window(window_attributes).unwrap());
+
+        #[cfg(not(target_arch = "wasm32"))]
+        {
+            // If we are not on web we can use pollster to
+            // await the 
+            self.state = Some(pollster::block_on(State::new(window)).unwrap());
+        }
+
+        #[cfg(target_arch = "wasm32")]
+        {
+            // Run the future asynchronously and use the
+            // proxy to send the results to the event loop
+            if let Some(proxy) = self.proxy.take() {
+                wasm_bindgen_futures::spawn_local(async move {
+                    assert!(proxy
+                        .send_event(
+                            State::new(window)
+                                .await
+                                .expect("Unable to create canvas!!!")
+                        )
+                        .is_ok())
+                });
+            }
+        }
+    }
+
+    #[allow(unused_mut)]
+    fn user_event(&mut self, _event_loop: &ActiveEventLoop, mut event: State) {
+        // This is where proxy.send_event() ends up
+        #[cfg(target_arch = "wasm32")]
+        {
+            event.window.request_redraw();
+            event.resize(
+                event.window.inner_size().width,
+                event.window.inner_size().height,
+            );
+        }
+        self.state = Some(event);
+    }
+
+    fn window_event(&mut self, event_loop: &ActiveEventLoop, window_id: winit::window::WindowId, event: WindowEvent) {
+    let state = match &mut self.state {
+            Some(canvas) => canvas,
+            None => return,
+        };
+
+        match event {
+            WindowEvent::CloseRequested => event_loop.exit(),
+            WindowEvent::Resized(size) => state.resize(size.width, size.height),
+            WindowEvent::RedrawRequested => {
+                state.render();
+            }
+            WindowEvent::KeyboardInput {
+                event:
+                    KeyEvent {
+                        physical_key: PhysicalKey::Code(code),
+                        state: key_state,
+                        ..
+                    },
+                ..
+            } => match (code, key_state.is_pressed()) {
+                (KeyCode::Escape, true) => event_loop.exit(),
+                _ => {}
+            },
+            _ => {}
+        }
+    } 
+
+    // ...
+}
+
+pub fn run() -> anyhow::Result<()> {
+    #[cfg(not(target_arch = "wasm32"))]
+    {
+        env_logger::init();
+    }
+    #[cfg(target_arch = "wasm32")]
+    {
+        console_log::init_with_level(log::Level::Info).unwrap_throw();
+    }
+
+    let event_loop = EventLoop::with_user_event().build()?;
+    let mut app = App::new(
+        #[cfg(target_arch = "wasm32")]
+        &event_loop,
+    );
+    event_loop.run_app(&mut app)?;
+
+    Ok(())
+}
+
+#[derive(Clone)]
+pub struct Buffer {
+    x: Vec<f32>,
+    y: Vec<f32>,
+    samples: i32,
+}
+
+pub struct Opt {
+    sampling: i32,
+    latency: i16,
+    volume: i32,            
+
+}
+
+impl Opt {
+    
+}
 
 fn main() {
     let mut buffer = Buffer{
         x: vec![],
         y: vec![],
+        samples: 1882,
     };
-
+    
+    let mut opt = Opt {
+        sampling: 100,
+        latency: 0,
+        volume: 100,
+    };
+    
     let (tx, rx) = mpsc::channel();
     let callback = move |audio_buffers: RUBuffers| {
         tx.send(audio_buffers);
     };
 
-
     let callback = rucallback!(callback);
     let mut ruhear = RUHear::new(callback);
-    
-
+    ruhear.start();
     loop {
-        ruhear.start();
-        buffer.x = rx.recv().unwrap()[0].clone();
-        buffer.y = rx.recv().unwrap()[1].clone();
+        buffer.samples = rx.recv().unwrap()[0].len() as i32;            
+        buffer.x = parse_samples(rx.recv().unwrap()[0].clone(), buffer.samples, opt.sampling, opt.volume);
+        buffer.y = parse_samples(rx.recv().unwrap()[1].clone(), buffer.samples, opt.sampling, opt.volume);
         show_raw(buffer.clone());
     }
-    //ruhear.start();
-    //std::thread::sleep(std::time::Duration::from_secs_f32(10.0));
-    //ruhear.stop(); 
+}
+fn parse_samples(samp: Vec<f32>, samples: i32, sampling: i32, volume: i32) -> Vec<f32> {
+    let mut output = vec![];
+    let skip_val = samples / sampling;
+    let mut x = 0 as usize;
+    while output.len() <= sampling as usize && samp.len() >= x {
+        let val = samp[x] * (volume/100) as f32;
+        output.push(val);
+        x += skip_val as usize;
+    }
+    //println!("Buff len: {:?}", output.len());
+    output
+}
 
+fn show_raw(mut buffer: Buffer) {
+    println!("X: {:?}, Samples: {:?}", buffer.x.pop(), buffer.samples);
+    println!("Y: {:?}, Samples: {:?}", buffer.y.pop(), buffer.samples);
+    println!("Buff len: {:?}", buffer.x.len());
+    //print!("{}[2J", 27 as char);
 }
 
 
-fn parse_stream(data: Vec<Vec<f32>>) -> Buffer {
-    println!("Samples: {:?}", &data[0].len());
-    let mut buff = Buffer {x: vec![], y: vec![]};
-    let value = data.clone();
-        for element in &value {
-        buff.x.push(element[0]);
-        //println!("X: {:?}", buffer.x.last());
-        buff.y.push(element[1]);
-        }
-    println!("Value Channel 0: {:?}", data[0].last());
-    println!("Value Channel 1: {:?}", data[1].last());
-    buff
-}
-fn show_raw(buffer: Buffer) {
-    println!("X: {:?}, Samples: {:?}", buffer.x.last(), buffer.x.len());
-    println!("Y: {:?}, Samples: {:?}", buffer.y.last(), buffer.y.len());
-    print!("{}[2J", 27 as char);
-}
