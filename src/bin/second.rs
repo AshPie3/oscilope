f01f743e02c195a727b48286472bc8dd8e8939ec
Nika <wonky.future@gmail.com>
2026-01-22 16:03:03 +0100

    point dimming and no queue

--- src/bin/second.rs
+++ src/bin/second.rs
@@ -15,6 +15,7 @@ pub struct Buffer {
 
 pub struct Opt {
     sampling: i32,
+    decay: f32,
     latency: i16,
     volume: f32,
     max_size: bool,
@@ -22,6 +23,16 @@ pub struct Opt {
     screen_height: i32,
 
 }
+static opt: Opt = Opt{
+        sampling: 1000,
+        decay: 0.7,
+        latency: 0,
+        volume: 400.0,
+        max_size: false,
+        screen_width: 640,// raylib::window::get_monitor_width(),
+        screen_height: 640,
+    };
+
 fn main() {
     let mut buffer = Buffer{
         x: vec![],
@@ -29,27 +40,15 @@ fn main() {
         samples: 1882,
     };
     
-    let mut opt = Opt {
-        sampling: 1000,
-        latency: 0,
-        volume: 400.0,
-        max_size: false,
-        screen_width: 640,// raylib::window::get_monitor_width(),
-        screen_height: 640,
-    };
-    let mut deque: VecDeque<(f32, f32)> = VecDeque::new();
-    
     let (tx, rx) = mpsc::channel();
     let callback = move |audio_buffers: RUBuffers| {
         tx.send(audio_buffers);
     };
-    let trail_len = 200 as usize;
     let point_radius = 2.5;
     let (mut rl, thread) = raylib::init()
         .size(opt.screen_width, opt.screen_height)
-        .title("Hello, World")
+        .title("Osciloscope")
         .build();
-    let mut last_clear_frame = 0;
     let mut ball_color = Color {
         r: 57,
         g: 89,
@@ -66,33 +65,34 @@ fn main() {
         buffer.samples = rx.recv().unwrap()[0].len() as i32;            
         buffer.x = parse_samples(rx.recv().unwrap()[0].clone(), buffer.samples, opt.sampling, opt.volume, opt.screen_width, opt.max_size);
         buffer.y = parse_samples(rx.recv().unwrap()[1].clone(), buffer.samples, opt.sampling, opt.volume, opt.screen_height, opt.max_size);
-        for i in 0..buffer.x.len() {
-            //deque.push_back((buffer.x.pop().unwrap(), buffer.y.pop().unwrap()));
-            deque.push_back((buffer.x[i], buffer.y[i]));
-        };
+        //let mut top_item = Vector2 {x: buffer.x.pop().unwrap(), y: buffer.y.pop().unwrap()};
+        //d.draw_circle_v(top_item, point_radius, ball_color);
         d.clear_background(Color {
-        r: 10,
-        g: 10,
-        b: 10,
-        a: 255,
+            r: 10,
+            g: 10,
+            b: 10,
+            a: 255,
         });
-        let mut i: usize = 0;
-        for i in 0..opt.sampling as usize{
-            let ratio = ((opt.sampling as f32 - i as f32) / opt.sampling as f32) ;
+        for i in 1..opt.sampling as usize-1 {
+            show_raw(buffer.clone());
+            let ratio = 1.0 - (opt.sampling as f32 - i as f32) / (opt.sampling as f32 * opt.decay);
             let mut trail_color = ball_color;
             trail_color.r = (ball_color.b as f32 * ratio) as u8;
             trail_color.g = (ball_color.g as f32 * ratio) as u8;
             trail_color.b = (ball_color.b as f32 * ratio) as u8;
-            let trail_radius = point_radius; // * ratio ;
-            let curr_item = deque.pop_front().unwrap();
-            if trail_color.r ==0 && trail_color.g == 0 {
+            let : (f32, f32) = buffer.x.pop()
+            let next_item = Vector2 {x: buffer.x.pop().unwrap(), y: buffer.y.pop().unwrap()};
+            let mut behind_item: Vector2;
+            if buffer.x.last() != None{
+                //behind_item = Vector2 {x: buffer.x.pop().unwrap(), y: buffer.y.pop().unwrap()};//deque.pop_front().unwrap();
+
             } else {
-              //  d.draw_line_v(Vector2 { x: deque.pop_front().unwrap().0, y: deque.pop_front().unwrap().1}, Vector2 { x: curr_item.0, y: curr_item.1 }, trail_color);
-                
-            d.draw_circle_v(Vector2 {x: curr_item.0/*buffer.x[i]*/ + 320.0, y: curr_item.1/*buffer.y[i]*/ + 320.0, }, trail_radius, trail_color);
+                behind_item = next_item;
+
             }
+               d.draw_circle_v(next_item, point_radius, trail_color);
+                
         };
-        show_raw(buffer.clone());
     }
 }
 fn parse_samples(samp: Vec<f32>, samples: i32, sampling: i32, mut volume: f32, screen: i32, max_size: bool) -> Vec<f32> {
@@ -110,7 +110,7 @@ fn parse_samples(samp: Vec<f32>, samples: i32, sampling: i32, mut volume: f32, s
     let skip_val = samples / sampling;
     let mut x = 0 as usize;
     while output.len() < sampling as usize && samp.len() >= x {
-        let val = samp[x] * volume;
+        let val = samp[x] * volume + screen as f32/2.0 ;
         output.push(val);
         x += skip_val as usize;
     }
