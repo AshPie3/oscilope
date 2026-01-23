f1b4608a83648f98d4b4bbefb06cf008deceb40c
Nika <wonky.future@gmail.com>
2026-01-23 16:48:22 +0100

    First working version imo

--- src/bin/second.rs
+++ src/bin/second.rs
@@ -28,9 +28,9 @@ pub struct Opt {
 fn main() {
     let mut opt =  Opt{
     sampling: 1000,
-    decay: 0.3,
+    decay: 15.0,
     latency: 0.0,
-    volume: 450.0,
+    volume: 350.0,
     max_size: false,
     screen_width: 800,
     screen_height: 800,
@@ -58,9 +58,9 @@ fn main() {
     opt.screen_width = rl.get_screen_width();
     opt.screen_height = rl.get_screen_height();
     let mut ball_color = Color {
-        r: 57,
+        r: 67,
         g: 255,
-        b: 20,
+        b: 40,
         a: 255,
     };
 
@@ -74,30 +74,32 @@ fn main() {
         let mut d = rl.begin_drawing(&thread);
         let mut recv = rx.recv().unwrap_or_default();
 
-        buffer.samples_x = recv[1].len() as i32;  
-        buffer.samples_y = recv[0].len() as i32;  
+        //buffer.samples_x = recv[1].len() as i32;  
+        //buffer.samples_y = recv[0].len() as i32;  
 
-        buffer.x =  std::mem::replace(&mut recv[1], vec![]);
-        buffer.y =  std::mem::replace(&mut recv[0], vec![]);
+        buffer.x =  std::mem::replace(&mut recv[0], vec![]);
+        buffer.y =  std::mem::replace(&mut recv[1], vec![]);
         
         buffer.x = parse_samples(buffer.x, opt.volume, opt.screen_width, opt.max_size);
         buffer.y = parse_samples(buffer.y, opt.volume, opt.screen_height, opt.max_size);
 
         //show_raw(buffer.clone());
-        d.draw_rectangle(0,0, opt.screen_width, opt.screen_height, Color::BLACK.alpha(0.7));
+        d.draw_rectangle(0,0, opt.screen_width, opt.screen_height, Color::BLACK.alpha(0.8));
         let mut last: Option<Vector2>= None;
-        for _ in 1..opt.sampling as usize-1 {
+        while buffer.x.len()>0 && buffer.y.len()>0{
+            //show_raw(buffer.clone());
             let sample = Vector2 {x: buffer.x.pop().unwrap(), y: buffer.y.pop().unwrap()};
-            //let ratio = 1.0 - (opt.sampling as f32 - i as f32) / (opt.sampling as f32 * opt.decay);
             let mut trail_color = ball_color;
             let previous_sample = last.unwrap_or(sample);
-            trail_color.a = ((opt.screen_width * opt.screen_height) as f32 * opt.decay / ((sample.x - previous_sample.x).powi(2) + (sample.y - previous_sample.y).powi(2)) as f32).clamp(0.0, 255.0) as u8 ;
+            trail_color.a = ((opt.screen_width * opt.screen_height) as f32 * (1.0/opt.decay) /
+                ((sample.x - previous_sample.x).powi(2) + (sample.y - previous_sample.y).powi(2)) as f32).clamp(10.0, 255.0) as u8 ;
             d.draw_line_ex(sample, previous_sample, point_radius, trail_color);
-            //d.draw_circle_v(sample, point_radius, trail_color);
+            //d.draw_circle_v(sample, point_radius, ball_color);
             last = Some(sample)
+            //} else {}
         };
-
     }
+    
 }
 fn parse_samples(samp: Vec<f32>, mut volume: f32, screen: i32, max_size: bool) -> Vec<f32> {
     if max_size{
@@ -109,8 +111,8 @@ fn parse_samples(samp: Vec<f32>, mut volume: f32, screen: i32, max_size: bool) -
 }
 
 fn show_raw(mut buffer: Buffer) {
-    println!("X: {:?}, Samples: {:?}", buffer.x.pop(), buffer.samples_x);
-    println!("Y: {:?}, Samples: {:?}", buffer.y.pop(), buffer.samples_y);
+    println!("X: {:?} ", buffer.x.pop());
+    println!("Y: {:?} ", buffer.y.pop());
     println!("Buff len: {:?}", buffer.x.len());
     //print!("{}[2J", 27 as char);
 }
